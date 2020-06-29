46c6cf62fe9c7f620eb763d54e7539d9af98a78e
Andy Elsbury <andy.elsbury@gmail.com>
2020-06-29 11:29:34 +0100

    Added a new app directory

---
+++ web-frameworks/express/applications/app/index.js
@@ -0,0 +1,35 @@
+const { join } = require('path')
+const express = require('express')
+const index = require('./routes/index')
+
+/*
+const http = require('http')
+http.createServer((req, res) => {
+  add extra methods and properties to req and res
+}))
+*/
+const app = express()
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+
+// Register express middleware
+/*
+const http = require('http')
+http.createServer((req, res) => {
+  call the middleware registered with app.use
+  wait for each piece of middleware to finish
+  before calling the next (wait for the next cb)
+}))
+*/
+if (dev) {
+  app.use(express.static(join(__dirname, 'public')))
+}
+
+// mount index at route path
+app.use('/', index)
+
+// Which port to listen to
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
